e111071cef868f2ba63f0d671527c8952b47fcf5
hllsebas <hllsebastian@gmail.com>
2021-06-10 07:35:14 -0500

    creando juego de ahorcado

---
+++ ahorcado_angularApp/ahorcado/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];   
+
+  constructor() {
+      console.log('Se acabo de crear la App Component')
+  }
+
+}
