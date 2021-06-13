341d8eafa9e49fbcc29b514ac3c3152c458ef29b
hllsebas <hllsebastian@gmail.com>
2021-06-13 00:52:35 -0500

    juego de ahorcado culmindao

--- ahorcado_angularApp/ahorcado/src/app/app.component.ts
+++ ahorcado_angularApp/ahorcado/src/app/app.component.ts
@@ -4,10 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent { 
 
-  palabra = 'AVE';
+  palabra = 'ARO';
   palabraOculta = '';
+  intentos= 0;
+  gano= false;
+  perdio= false;
 
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
             'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
@@ -19,6 +22,7 @@ export class AppComponent {
 
   comprobar(letra: any) {
 
+    this.existeLetra(letra);
     const palabraOcultaArr= this.palabraOculta.split(' ');
     
     for(let i= 0; i< this.palabra.length; i++) {
@@ -28,6 +32,34 @@ export class AppComponent {
     }
 
     this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGana();
+  }
 
+verificaGana() {
+
+  const palabraArr = this.palabraOculta.split(' ');
+  const palabraEvaluar= palabraArr.join('');
+  console.log(palabraEvaluar);
+
+  if (palabraEvaluar=== this.palabra) {
+    this.gano= true;
+    console.log('Ganaste!!');
+  }
+
+  if (this.intentos >= 9) {
+    this.perdio= true;
+    console.log('Que pena, has perdido')
+  }
+}
+
+
+  existeLetra (letra: any) {  
+
+    if (this.palabra.indexOf(letra) >=0 ) {
+      //console.log('La letra existe: ' + letra);
+      } else {
+          //console.log('La letra no existe: ' + letra);
+          this.intentos ++;
+      } 
   }
 }
